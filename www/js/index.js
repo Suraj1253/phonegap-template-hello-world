108f5e6dcafb6d032c5d40b877012f934d16ad91
Suraj Batra <surajbatra240@gmail.com>
2020-12-22 17:22:23 +0530

    WWW folder has modified

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,41 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+document.addEventListener('deviceready', onMyDeviceReady, false);
 
-        console.log('Received Event: ' + id);
-    }
-};
+function onMyDeviceReady() {
+    
+    document.getElementById('message').innerHTML= 'Welcome to Cross Platform App development';
+
+}
+
+document.addEventListener('pause', onPause, false);
+
+function onPause(){
+         alert('OnPause function is called');
+}
+
+document.addEventListener('resume', onResume, false);
+
+function onResume(){
+         alert('OnResume function is called');
+}
+
+document.addEventListener('Backbutton', onPressingBackbutton, false);
+
+function onPressingBackbutton(e) {
+         e.preventDefault();
+         alert('onPressingBackButton function is called');
+}
+
+document.addEventListener('menubutton', onMenubuttonPressed, false);
+
+function onmenubuttonPressed(e) {
+         alert('onmenubuttonPressed function is called');
+}
+
+document.addEventListener('searchbutton', onsearchbutton, false);
+
+function onsearchbutton(e) {
+         console.log('This is search button event');
+}
+
+
